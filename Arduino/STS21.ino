58220d1377b7797355e18953dd85b20c7909be2c
Yadwinder Singh <ryker1990@gmail.com>
2016-04-28 14:30:53 +0530

    Update STS21.ino

--- Arduino/STS21.ino
+++ Arduino/STS21.ino
@@ -4,7 +4,7 @@
 // This code is designed to work with the STS21_I2CS I2C Mini Module available from ControlEverything.com.
 // https://www.controleverything.com/content/Temperature?sku=STS21_I2CS#tabs-0-product_tabset-2
 
-#include <Wire.h>;
+#include <Wire.h>
 
 // STS21 I2C address is 0x4A(74)
 #define addr 0x4A
